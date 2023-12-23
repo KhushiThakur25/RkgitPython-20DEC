d514783b3236fbd24f57369d9a877a984fceb161
KhushiThakur25 <113238209+KhushiThakur25@users.noreply.github.com>
2023-12-23 17:47:03 +0530

    Add files via upload

---
+++ class15.py
@@ -0,0 +1,16 @@
+'''1.print-single line
+2.print-multiple lines
+3.single line comment
+4.multiple line comment'''
+
+'''x = 10
+y = 20
+z = 30
+s = 40'''
+x,y,z,s = 10,20,30,40
+print(x,y,z,s)
+print("""python is a programming language..
+      lksdhfdfgd
+      ksdnffjdfvhifug
+      jknbfidhud""")
+print(5)
