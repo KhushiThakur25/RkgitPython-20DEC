90b9843044174a34db7e2c8dcede78dfe66c238a
KhushiThakur25 <113238209+KhushiThakur25@users.noreply.github.com>
2024-01-02 18:01:01 +0530

    Add files via upload

---
+++ RKGITFor Loop.py
@@ -0,0 +1,50 @@
+'''for i in range(5):
+    print("I'm in the loop")
+print("I'm out of the loop")
+
+i = 0
+flag = True'''
+'''while i < 10:
+    print(i)
+    i += 1'''
+
+
+'''while True:
+    print(i)
+    i += 1
+    if i > 5:
+        break'''
+'''while flag:
+    print(i)
+    i += 1
+    if i > 5:
+        flag = False'''
+#Table
+a = int(input("Enter the value a"))
+'''b = int(input("Enter the value b"))
+c = int(input("Enter the value c"))'''
+'''for i in range(1,11):
+    print(f"{n} * {i} = {n*i}")
+
+i = 1
+while i<11:
+    print(f"{n} * {i} = {n*i}")
+    i += 1'''
+
+'''if n%2 == 0:
+    print("number is even..")
+else:
+    print("number is odd..")'''
+
+'''if a > b and a > c:
+    print("a is greatest")
+elif b > a and b > c:
+    print("b is greatest")
+else:
+    print("c is greatest")'''
+
+result = "even" if a%2 == 0 elif  "odd"
+print(result,"number")
+
+
+
